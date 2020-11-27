1cc6725856e935fbf6595e2eaf9713c1b01ae453
Angel Rodriguez <angelr1076@gmail.com>
2020-11-27 15:35:36 -0500

    New local settings file added

--- wiki/settings.py
+++ wiki/settings.py
@@ -132,3 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+try:
+    from .local_settings import *
+except ImportError:
+    print("No local file available. You should be on production.")
